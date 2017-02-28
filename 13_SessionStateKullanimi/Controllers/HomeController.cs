fe80327bd5f0d624c157fee835d19b6597646a09
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-28 16:38:58 +0300

    Session Kullanimi

---
+++ 13_SessionStateKullanimi/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _13_SessionStateKullanimi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string text) //kullanicidan aldigim bilgi
+        {
+            Session["deger"] = text;  //session obje tutar.
+            return RedirectToAction("Index2");
+        }
+
+        public ActionResult Index2()
+        {
+            ViewBag.Deger = Session["deger"].ToString();
+            return View();
+        }
+    }
+}
